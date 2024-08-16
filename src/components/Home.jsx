696e43e7faf71a22855fc222fc74cf1fed932b36
Shayan-Mst <shnbackup007@gmail.com>
2024-08-16 03:51:46 +0330

    added some feature but bugs not fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from './Header'
 import Sidebar from './Sidebar'
 import NavPlayer from './NavPlayer'
+import Body from './Body'
 
 
 const Home = () => {
@@ -9,6 +10,7 @@ const Home = () => {
     <>
     <Header/>
     <Sidebar/>
+    <Body/>
     <NavPlayer/>
    
     </>
